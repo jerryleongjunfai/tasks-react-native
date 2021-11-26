7bfe3b21ce4f9c3c84a2929db5da35cd5de712d4
Jerry Leong <junfai2007@gmail.com>
2021-11-26 20:34:42 +0800

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, View, Keyboard, TouchableOpacityBase, TouchableWithoutFeedback, TouchableOpacity} from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handlerAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss()
+    }}>
+      <View style={styles.container}>
+        {/* Today's Tasks */}
+        <View style={styles.taskWrapper}>
+          <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+          <View style={styles.items}>
+            {/* This is where the tasks will go! */}
+            {
+              taskItems.map((item, index) => {
+                return (
+                  <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                    <Task text={item}/>
+                  </TouchableOpacity>
+                )
+              })
+            }
+          </View>
+        </View>
+
+        {/* Write a task */}
+        {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
+
+        <KeyboardAvoidingView
+          behavior={Platform.OS=="ios" ? "padding" : "height"}
+          style={styles.writeTaskWrapper}>
+            <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)}/>
+
+            <TouchableOpacity onPress={() => handlerAddTask()}>
+              <View style={styles.addWrapper}>
+                <Text style={styles.addText}>➕</Text>
+              </View>
+            </TouchableOpacity>
+          </KeyboardAvoidingView>
+      </View>
+    </TouchableWithoutFeedback>
   );
-}
+} 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 29,
+    fontWeight: 'bold'
+  },
+  items:{
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  }
+
 });
---
+++ components/Task.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { View, Text, StyleSheet} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
+const Task = (props) => {
+    return(
+        <View style={styles.item}>
+            <View style={styles.itemLeft}>
+                <View style={styles.square}></View>
+                <Text style={styles.itemText}>{props.text}</Text>
+            </View>
+            <View>
+                <MaterialIcons name='delete' size={20} color='#333'/>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: '#FFF',
+        padding: 15,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 20
+    },
+    itemLeft: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap'
+    },
+    square: {
+        width: 24,
+        height: 24,
+        backgroundColor: '#55BCF6',
+        opacity: 0.4,
+        borderRadius: 5,
+        marginRight: 15
+    },
+    itemText: {
+        maxWidth: '80%'
+    },
+});
+
+export default Task;
